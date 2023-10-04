5e6725d54fb82408f68b4fc0c3721588c2686af4
XIAOYixuan <xiaoyx.bit@gmail.com>
2023-10-04 09:34:47 +0200

    pdf, enable right click and menus

--- client/src/components/pdfViewer/index.tsx
+++ client/src/components/pdfViewer/index.tsx
@@ -2,12 +2,15 @@ import { Document, Page } from 'react-pdf/dist/esm/entry.vite';
 import 'react-pdf/dist/esm/Page/TextLayer.css';
 import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
 import type { PDFDocumentProxy } from 'pdfjs-dist';
-import { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import eventEmitter from '../../utils/eventEmitter';
 import PageSpin from '../pageSpin';
 
-export default function PdfViewer({ file }: { file: Blob }) {
+export default function PdfViewer({ file, onTextSelect }: { file: Blob, onTextSelect: (selectedText: string) => void }) {
   const [numPages, setNumPages] = useState<number>();
+  const [showContextMenu, setShowContextMenu] = useState<boolean>(false);
+  const [contextMenuPosition, setContextMenuPosition] = useState<{ x: number, y: number }>({ x: 0, y: 0 });
+  const [selectedText, setSelectedText] = useState<string>('');
   const pdfRef = useRef<HTMLDivElement>(null);
 
   function scrollToPage(meta: { pageNo: number; time: number }) {
@@ -32,6 +35,47 @@ export default function PdfViewer({ file }: { file: Blob }) {
     setNumPages(nextNumPages);
   }
 
+  function onContextMenu(e: React.MouseEvent) {
+    e.preventDefault();
+    setSelectedText(window.getSelection()?.toString() || '');
+    setShowContextMenu(true);
+    setContextMenuPosition({ x: e.clientX, y: e.clientY });
+  }
+  // five main functions:
+  // func: explain selected text with examples
+  // func: simply selected text
+  // func: detect jargon
+  // func: exemplify jargon
+  function onExplainClick() {
+    //alert(selectedText);
+    onTextSelect("::explain::" + selectedText);
+    setShowContextMenu(false);
+  }
+  
+  function onSimplifyClick() {
+    //alert(selectedText);
+    onTextSelect("::simplify::" + selectedText);
+    setShowContextMenu(false);
+  }
+  
+  function onDetectClick() {
+    //alert(selectedText);
+    onTextSelect("::detect::" + selectedText);
+    setShowContextMenu(false);
+  }
+  
+  function onExemplifyClick() {
+    //alert(selectedText);
+    onTextSelect("::exemplify::" + selectedText);
+    setShowContextMenu(false);
+  }
+  
+  function onAddChatClick() {
+    //alert(selectedText);
+    onTextSelect(selectedText);
+    setShowContextMenu(false);
+  }
+
   return (
     <Document
       inputRef={pdfRef}
@@ -45,8 +89,27 @@ export default function PdfViewer({ file }: { file: Blob }) {
       }}
     >
       {Array.from(new Array(numPages), (_el, index) => (
-        <Page width={690} key={`page_${index + 1}`} pageNumber={index + 1} />
+        <div key={`page_${index + 1}`} onContextMenu={onContextMenu}>
+        <Page 
+          width={690} 
+          key={`page_${index + 1}`} 
+          pageNumber={index + 1} />
+        </div>
       ))}
+      {showContextMenu && (
+        <div style={{ 
+          position: 'fixed', 
+          top: contextMenuPosition.y, 
+          left: contextMenuPosition.x,
+          display: 'flex',
+          flexDirection: 'column' }}>
+          <button onClick={onExplainClick}>Explain text</button>
+          <button onClick={onExemplifyClick}>Show an example</button>
+          <button onClick={onDetectClick}>Detect jargons</button>
+          <button onClick={onSimplifyClick}>Simplify text</button>
+          <button onClick={onAddChatClick}>Add to chat</button>
+        </div>
+      )}
     </Document>
   );
 }
